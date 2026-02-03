f55907efb92b848c6fc67012b8e350e6739ba7bc
meenu10802 <meenu10802>
2026-02-03 17:08:10 +0530

    [Meenakshi]: UC6 Add collection operations

--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /*
  * VesselUtil class
  * ----------------
- * UC5: Provides storage and retrieval for UI.
+ * UC6: Uses Collections framework to sort vessels
+ * based on average speed.
  */
 public class VesselUtil {
 
@@ -14,7 +17,19 @@ public class VesselUtil {
         vesselList.add(vessel);
     }
 
-    public List<Vessel> getAllVessels() {
+    // Sort vessels by average speed (descending)
+    public List<Vessel> sortVesselsBySpeed() {
+
+        Collections.sort(vesselList, new Comparator<Vessel>() {
+            @Override
+            public int compare(Vessel v1, Vessel v2) {
+                return Double.compare(
+                        v2.getAverageSpeed(),
+                        v1.getAverageSpeed()
+                );
+            }
+        });
+
         return vesselList;
     }
 }
